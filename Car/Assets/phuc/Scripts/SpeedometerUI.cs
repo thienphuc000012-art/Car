e9d515641487e77e931cc2bf7c7e25fb8ae22583
CaoThienPhuc <thienphuc000012@gmail.com>
2026-05-23 12:35:40 +0700

    .

---
+++ Car/Assets/phuc/Scripts/SpeedometerUI.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class SpeedometerUI : MonoBehaviour
+{
+    public Rigidbody carRigidbody;   
+    public RectTransform needle;     
+    public TMP_Text speedText;       
+
+    public float maxSpeed = 200f;   
+    public float minAngle = -130f;   
+    public float maxAngle = 130f;    
+
+    void Update()
+    {
+       
+        float speed = carRigidbody.linearVelocity.magnitude * 3.6f;
+
+        float angle = Mathf.Lerp(minAngle, maxAngle, speed / maxSpeed);
+
+        needle.localRotation = Quaternion.Euler(0, 0, angle);
+
+        if (speedText != null)
+            speedText.text = Mathf.RoundToInt(speed) + " km/h";
+    }
+}
---
+++ Car/Assets/phuc/UI/ville_seppanen_racing_hud_asset_pack/copyright_readme_racing_hud.txt
@@ -0,0 +1,11 @@
+Racing HUD Asset Pack
+
+License
+This asset pack is released under Creative Commons Attribution 4.0 (CC BY 4.0).
+You are free to use, modify, and redistribute the assets, including in commercial projects.
+Attribution is required.
+
+Credit:
+Assets by Ville Seppänen https://www.villeseppanen.com
+
+If you use this kit in a project, I’d love to see it!
