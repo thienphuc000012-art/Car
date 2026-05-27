ff3fe232e03281fedc7cddc90bed81239ff5587c
CaoThienPhuc <thienphuc000012@gmail.com>
2026-05-27 16:03:11 +0700

    .

--- Car/Assets/phuc/Scripts/CarCameraController.cs
+++ Car/Assets/phuc/Scripts/CarCameraController.cs
@@ -1,30 +1,38 @@
-﻿using UnityEngine;
-using Unity.Cinemachine; 
+﻿//using UnityEngine;
+//using Unity.Cinemachine;
 
-public class CarCameraController : MonoBehaviour
-{
-    public Rigidbody carRb;
-    public CinemachineCamera vcam;
-    private CinemachineFollow follow;
-    private CinemachineImpulseSource impulseSource; 
-    public float baseFOV = 60f;
-    public float maxFOV = 80f;
-    public float speedForMaxFOV = 200f;
+//public class CarCameraController : MonoBehaviour
+//{
+//    public Rigidbody carRb;
+//    public CinemachineCamera vcam;
+//    private CinemachineFollow follow;
 
-    void Start()
-    {
-        follow = vcam.GetComponent<CinemachineFollow>();
-        impulseSource = vcam.GetComponent<CinemachineImpulseSource>();
-    }
+//    public float baseFOV = 60f;
+//    public float maxFOV = 80f;
+//    public float speedForMaxFOV = 200f;
 
-    void Update()
-    {
-        float speed = carRb.linearVelocity.magnitude * 3.6f; 
-        float targetFOV = Mathf.Lerp(baseFOV, maxFOV, speed / speedForMaxFOV);
-        vcam.Lens.FieldOfView = targetFOV;
-        if (impulseSource != null && speed > 100f)
-        {
-            impulseSource.GenerateImpulse();
-        }
-    }
-}
+//    public float baseDistance = 10f;
+//    public float minDistance = 6f;
+
+//    void Start()
+//    {
+//        follow = vcam.GetComponent<CinemachineFollow>();
+//    }
+
+//    void Update()
+//    {
+//        float speed = carRb.linearVelocity.magnitude * 3.6f;
+
+//        // FOV zoom theo tốc độ
+//        float targetFOV = Mathf.Lerp(baseFOV, maxFOV, speed / speedForMaxFOV);
+//        vcam.Lens.FieldOfView = targetFOV;
+
+//        // Camera tiến gần theo tốc độ
+//        if (follow != null)
+//        {
+//            Vector3 offset = follow.FollowOffset;
+//            offset.z = Mathf.Lerp(-baseDistance, -minDistance, speed / speedForMaxFOV);
+//            follow.FollowOffset = offset;
+//        }
+//    }
+//}
